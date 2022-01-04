b5dff4c050dc78f94e953b1c2f8107217784a8a6
dizhoykin <dizhoykin@gmail.com>
2022-01-05 01:09:37 +0700

    Transfer user and pc choose to the winner.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const buttons = document.querySelectorAll('.pick-one button');
 const pc_choose = buttons[Math.floor(Math.random()*buttons.length)].querySelector('img');
+localStorage.clear();
+var user_choose;
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const user_choose = button.querySelector('img');
+    user_choose = button.querySelector('img');
+    localStorage.setItem('user_choose_alt', user_choose.alt);
+    localStorage.setItem('user_choose_src', user_choose.src);
     document.location.href = 'winner.html';
   });
 });
+
+localStorage.setItem('pc_choose_alt', pc_choose.alt);
+localStorage.setItem('pc_choose_src', pc_choose.src);
--- winner.js
+++ winner.js
@@ -1,16 +1,15 @@
 const playButton = document.querySelector('.play-again');
-const user_pick_img = document.querySelector('.you-pick img');
+const user_pick_img = document.querySelector('.your-pick img');
 const pc_pick_img = document.querySelector('.computer-pick img');
 const winner = document.querySelector('.winner');
-console.log(pc_choose);
 
-user_pick_img.alt = user_choose.alt;
-user_pick_img.src = user_choose.src;
+user_pick_img.alt = localStorage.getItem('user_choose_alt');
+user_pick_img.src = localStorage.getItem('user_choose_src');
 
-pc_pick_img.alt = pc_choose.alt;
-pc_pick_img.src = pc_choose.src;
+pc_pick_img.alt = localStorage.getItem('pc_choose_alt');
+pc_pick_img.src = localStorage.getItem('pc_choose_src');
 
-if (user_choose.alt == pc_choose.alt) {
+if (user_pick_img.alt == pc_pick_img.alt) {
   winner.classList.remove('computer-wins');
 }
 
